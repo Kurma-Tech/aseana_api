25b7bdc6495afc220e5e21782185e984e66b2b21
suzanpradhan <niweshs@gmail.com>
2022-09-22 13:43:28 +0545

    Initial commit

---
+++ constants.py
@@ -0,0 +1,9 @@
+
+# aseanaapiproject - base64 utf-8
+AUTH_TOKEN = 'YXNlYW5hYXBpcHJvamVjdA=='
+
+# data fields on the payload
+JSON_DATA = ['auth_token', 'column_name', 'years_to_predict', 'data_to_predict']
+
+# debug for showing the graph result for the prediction
+SHOW_GRAPH = False
