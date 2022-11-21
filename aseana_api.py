36b73a5501078f62be3e60797a07f469c8bddc27
suzanpradhan <74817169+suzanpradhan@users.noreply.github.com>
2022-11-21 17:51:48 +0545

    Update aseana_api.py

--- aseana_api.py
+++ aseana_api.py
@@ -17,13 +17,8 @@ def predict():
   try:
     mydb = mysql.connector.connect(
     host="13.250.218.164",
-    # host="localhost",
-    port="3306",
     user="asean",
-    # user="root",
     password="Zenith4780@",
-    # password="",
-    # database="asean"
     database="aseana_db"
     )
 
@@ -31,7 +26,7 @@ def predict():
 
     request_json_data = request.get_json()
 
-    queryString = f"SELECT month_and_year, COUNT(*) FROM {request_json_data['type']} "    
+    queryString = f"SELECT year, COUNT(*) FROM {request_json_data['type']} "    
 
     if (request_json_data["country_id"] is not None):
       queryString = queryString + f"WHERE country_id = {request_json_data['country_id']} "
@@ -42,32 +37,29 @@ def predict():
       if (request_json_data["classification_id"] is not None):
         queryString = queryString + f"WHERE parent_classification_id = {request_json_data['classification_id']} "
 
-    queryString = queryString + "GROUP BY month_and_year ORDER BY month_and_year"
+    queryString = queryString + "GROUP BY year ORDER BY year"
 
     mycursor.execute(queryString)
 
     myresult = mycursor.fetchall()
 
     dataframe = pd.DataFrame()
-    dataframe['DateTime'] = [i[0] for i in myresult]
+    dataframe['Year'] = [i[0] for i in myresult]
 
     dataframe['Count'] = [i[1] for i in myresult]
 
-    dataframe['DateTime'] = pd.to_datetime(dataframe['DateTime']).dt.date
+    # dataframe['DateTime'] = pd.to_datetime(dataframe['DateTime']).dt.date
 
-    dataframe = dataframe.groupby('DateTime', as_index=False).sum()
-    dataframe.sort_values(by="DateTime", inplace=True)
+    # dataframe = dataframe.groupby('DateTime', as_index=False).sum()
+    dataframe.sort_values(by="Year", inplace=True)
 
-    dataframe.index = pd.DatetimeIndex(dataframe['DateTime'])
+    # dataframe.index = pd.DatetimeIndex(dataframe['DateTime'])
 
-    # dataframe = dataframe.reindex(idx, fill_value=0)
+    # dataframe.drop(['DateTime'], axis=1, inplace=True)
+    # dataframe = dataframe.rename_axis('DateTime').reset_index()
 
-    # dataframe['DateTime'] = dataframe.index
-    dataframe.drop(['DateTime'], axis=1, inplace=True)
-    dataframe = dataframe.rename_axis('DateTime').reset_index()
-
-    dataframe['Year'] = [i.year for i in dataframe['DateTime']]
-    dataframe= dataframe.groupby(["Year"], as_index=False).sum()
+    # dataframe['Year'] = [i.year for i in dataframe['DateTime']]
+    # dataframe= dataframe.groupby(["Year"], as_index=False).sum()
     # dataframe['MA12'] = dataframe['Count'].rolling(3).mean()
 
     for i in range(10):
@@ -88,4 +80,4 @@ def predict():
   except Exception as e:
     return jsonify({'status': HTTPStatus.INTERNAL_SERVER_ERROR, 'message': "Not enough data to forcast.", 'exception': str(e.__class__), "success": False})
 
-# app.run(debug=True)
+app.run(debug=True)
